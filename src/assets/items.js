9125c45e200f57141786d6197163ba793afaa77e
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-01-21 20:11:17 +0000

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,60 +1,27 @@
 import website from '../assets/website.png'
-import instagram from '../assets/instagram.png'
-import appstore from '../assets/playstore.png'
-import youtube from '../assets/youtube.jpeg'
-import dribbble from '../assets/dribbble.png'
+import prodhunt from '../assets/prodhunt.png'
 import github from '../assets/github.png'
-import telegram from '../assets/telegram.png'
-import twitter from '../assets/twitter.png'
 
 const items = [{
     "title": "Website",
-    "subtitle": "Look at my work!",
+    "subtitle": "Look at my site!",
     "image": website,
-    "link": "https://sagniksahoo.me" //your personal website or portfolio  link
+    "link": "https://nayanpatel.net" //my personal website
 },
 {
     "title": "GitHub",
-    "subtitle": "@devgossips | 🏠 of my open-source projects",
+    "subtitle": "@PatelN123 | 🏠 of my open-source projects",
     "image": github,
-    "link": "https://github.com/devgossips" //Github Profile link
+    "link": "https://github.com/pateln123" //my Github Profile link
 },
 {
-    "title": "Instagram",
-    "subtitle": "@heysagnik | Hub of my awesome 😎 pics",
-    "image": instagram,
-    "link": "https://instagram.com/heysagnik" //instagram profile link 
+    "title": "Product Hunt",
+    "subtitle": "@nayanpatel",
+    "image": prodhunt,
+    "link": "https://www.producthunt.com/@nayanpatel" //my ProductHunt Profile link
 },
-{
-    "title": "Twitter",
-    "subtitle": "@heysagnik | Don't forget to follow me 😉",
-    "image": twitter,
-    "link": "https://twitter.com/heysagnik"// twitter profile link 
-},
-{
-    "title": "Apps",
-    "subtitle": "Hub of my awesome 🔥 Apps",
-    "image": appstore,
-    "link": "#"//
-},
-{
-    "title": "YouTube",
-    "subtitle": "@Sagnik Sahoo | Official channel of mine",
-    "image": youtube,
-    "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"//youtube channel link 
-},
-
-{
-    "title": "Dribbble",
-    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
-    "image": dribbble,
-    "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
-},
-{
-    "title": "Telegram",
-    "subtitle": "@virtuonic | Chat with me instantly, Don't hesitate! ",
-    "image": telegram,
-    "link": "https://telegram.me/heysagnik" //Telegram Pofile 
-}]
+              
+              
+              ]
 
 export default items
